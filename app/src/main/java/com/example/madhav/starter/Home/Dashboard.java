f820fc9959d5b8651ddba0e92c9a7515973976b9
A Madhav Bharadwaj <madhavbharadwaj1995@gmail.com>
2018-09-26 00:19:48 +0530

    navigation drawer
    3 fragments added
    all projects,mca,icl

--- app/src/main/java/com/example/madhav/starter/Home/Dashboard.java
+++ app/src/main/java/com/example/madhav/starter/Home/Dashboard.java
@@ -1,17 +1,102 @@
 package com.example.madhav.starter.Home;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.madhav.starter.R;
 import com.example.madhav.starter.Splash_Screen.LaunchScreenActivity;
 
-public class Dashboard extends AppCompatActivity {
+public class Dashboard extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
 
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        all_projects fir = new all_projects();
+
+        setTitle("All Projects");
+
+        android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.fragment,fir).commit();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_all_projects) {
+            setTitle("All Projects");
+            all_projects fir = new all_projects();
+            android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.fragment,fir).commit();
+
+
+
+
+        }
+        else if (id == R.id.nav_mca) {
+            // Handle the cgpa action
+            setTitle("MCA");
+            mca_projects sec = new mca_projects();
+            android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.fragment,sec).commit();
+
+        }
+        else if (id == R.id.nav_icl) {
+            // Handle the cgpa action
+            setTitle("ICL");
+            icl_projects sec = new icl_projects();
+            android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.fragment,sec).commit();
+
+        }
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
---
+++ app/src/main/java/com/example/madhav/starter/Home/mca_projects.java
@@ -0,0 +1,30 @@
+package com.example.madhav.starter.Home;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.madhav.starter.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class mca_projects extends Fragment {
+
+
+    public mca_projects() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_mca_projects, container, false);
+    }
+
+}
