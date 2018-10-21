73f6e0ccd789010b792d0eeb33971dbc62d3255e
ABHILASH N <abhilashgowda.1996@gmail.com>
2018-10-21 15:43:22 +0530

    get avilable domain
    all the domains will be listed

--- api/routes/domainroutes.js
+++ api/routes/domainroutes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const Domain = require("../models/domain");
 
+
+//API TO ADD DOMAIN
+
 router.post("/", (req, res, next) => {
     const domain = new Domain({
       domain: req.body.domain
@@ -27,4 +30,28 @@ router.post("/", (req, res, next) => {
       });
   });
   
+//API TO GET ALL AVAILABLE DOMAIN
+
+router.get("/", (req, res, next) => {
+    Domain.find()
+   .select("domain")
+    .exec()
+    .then(docs => {
+      const response = {
+        TOTAL_NO_OF_DOMAINS: docs.length,
+            DOMAIN_DETAILS: docs
+         };
+      
+      res.status(200).json(response);
+      
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+  });
+
+
   module.exports = router;
